07145f92107f7e7018e8631868e586991324ae3f
Milena <morozowamilena@yandex.ru>
2019-06-06 21:27:56 +0300

    add two strings

--- TestProject/TestProject.cpp
+++ TestProject/TestProject.cpp
@@ -2,10 +2,12 @@
 //
 
 #include <iostream>
+#include <stdlib.h>
 
 int main()
 {
     std::cout << "Hello World!\n"; 
+	printf("SAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAshen'ka :)");
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
