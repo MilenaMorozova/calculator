e0aa3a5a667ad7cceac99a3efc95b80670d52905
Milena <morozowamilwna@yandex.ru>
2019-06-07 17:32:28 +0300

    I create a new project

---
+++ calculator/calculator/calculator.cpp
@@ -0,0 +1,10 @@
+﻿// calculator.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Let's start"); 
+}
+
