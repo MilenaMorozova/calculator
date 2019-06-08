989b464bfc1c7fbfb36082c23d8b90020d58770a
IMashtakov <mashtakovigor@mail.ru>
2019-06-08 18:59:23 +0300

    Add numbers for display and add new function for unput to Display

--- calculator/calculator/calculator.cpp
+++ calculator/calculator/calculator.cpp
@@ -10,7 +10,8 @@
 //Screen dimension constants
 const int SCREEN_WIDTH = 580;
 const int SCREEN_HEIGHT = 700;
-
+const int maxAfterPoint = 8;
+int col0 = 0;
 //--------GLOBAL VARIABLES------
 //The window we'll be rendering to
 SDL_Window* gWindow = NULL;
@@ -136,6 +137,139 @@ void close()
 	IMG_Quit();
 	SDL_Quit();
 }
+int SymbolsAfterComma(double number)
+{
+	//counts the symbols after comma
+	number += pow(10,-maxAfterPoint-2);
+	int col = 0;
+	for (int i = 1; i < maxAfterPoint+1; i++)
+	{
+		number -= (int)(number);
+		number *= 10;
+		if (((int)number % 10) != 0)
+			col = i;
+	}
+	return col+col0;
+}
+int SymbolsBeforeComma(double number)
+{
+	//counts the symbols before comma
+	int i = 0;
+	int N = (int)(number);
+	while (N)
+	{
+		N /= 10;
+		i++;
+	}
+	return i;
+}
+void UpdateDisplay(double AllNumber, int comma, SDL_Texture** numbers)
+{
+	//variable calculation
+	int nullposx = (int)(SCREEN_WIDTH * 0.815);
+	int nullposy = (int)(SCREEN_HEIGHT * 0.067);
+	int numbsizex = (int)(SCREEN_WIDTH * 0.1);
+	int numbsizey = (int)(numbsizex * 3 / 2);
+	int afcom = SymbolsAfterComma(AllNumber);
+	int befcom = SymbolsBeforeComma(AllNumber);
+	int colvivod = 0;
+	double Allnumb = AllNumber - (int)AllNumber;
+
+	//Clean display
+	for (int i = 0; i < 8; i++)
+	{
+		loadFromFile(nullposx - (i * numbsizex), nullposy, numbers[11], gRenderer, numbsizex, numbsizey);
+	}
+	//output on display 0
+	for (int i = 0; i < col0; i++)
+	{
+		loadFromFile(nullposx - (colvivod * numbsizex), nullposy, numbers[0], gRenderer, numbsizex, numbsizey);
+		colvivod++;
+	}
+	//output on display symbols after comma
+	for (; afcom-col0 > 0; afcom--)
+	{
+		int numb = ((int)(Allnumb * pow(10, afcom))) % 10;
+		loadFromFile(nullposx - (colvivod * numbsizex), nullposy, numbers[numb], gRenderer, numbsizex, numbsizey);
+		colvivod++;
+	}
+	
+
+	//output on display comma
+	if(comma)
+		loadFromFile(nullposx - (colvivod * numbsizex)+ (numbsizex / 2), nullposy, numbers[10], gRenderer, numbsizex/2, numbsizey);
+	//output on display symbols before comma
+	if (befcom != 0)
+	{
+		int allnum = floor(AllNumber);
+		while(allnum)
+		{
+			int numb = allnum % 10;
+			loadFromFile(nullposx - (colvivod * numbsizex)-((numbsizex / 2)*comma), nullposy, numbers[numb], gRenderer, numbsizex, numbsizey);
+			allnum /= 10;
+			colvivod++;
+		}
+	}
+	else
+	{
+		loadFromFile(nullposx - (colvivod * numbsizex) - ((numbsizex / 2) * comma), nullposy, numbers[0], gRenderer, numbsizex, numbsizey);
+	}
+
+
+	SDL_RenderPresent(gRenderer);
+}
+void ClickToNumber(int input,int* comma,double* AllNumber, SDL_Texture** numbers)
+{
+	if (input < 0 || input>11)
+		return;
+	int AfCom = SymbolsAfterComma(*AllNumber);
+	if (input == 11)
+	{//input Backspace
+		if (AfCom == 0)
+		{
+			if (*comma)
+				* comma = 0;
+			else
+			*AllNumber = (*AllNumber - (((int)(*AllNumber))% 10)) / 10;
+		}
+		else
+		{
+			*AllNumber = *AllNumber - ((((int)(*AllNumber * pow(10, AfCom))) % 10) * pow(10, -AfCom));
+		}
+		
+	}
+	else if(input == 10)
+	{//input comma
+		if(AfCom == 0)
+		*comma = !*comma;
+		else
+		{
+			return;
+		}
+	}
+	else
+	{//input number
+		if (*comma == 0)
+		{
+			*AllNumber = *AllNumber * 10 + input;
+		}
+		else
+		{
+			if (AfCom == maxAfterPoint)
+				return;
+			if (input == 0)
+				col0++;
+			else
+			{
+				*AllNumber = *AllNumber + (input * pow(10, -AfCom - 1));
+				col0 = 0;
+			}
+		}
+	}
+	UpdateDisplay(*AllNumber, *comma, numbers);
+}
+
+
 
 int main(int argc, char* args[])
 {
@@ -145,14 +279,28 @@ int main(int argc, char* args[])
 		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
 	}	else {
 
+		double AllNumber = 0;
+		int comma = 0;
+		
 		//background of calculator
 		SDL_Texture* background = loadTexture("base.png");
+		//numbers for display
+		SDL_Texture* numbers[12] = { loadTexture("null.jpg"),loadTexture("one.jpg"),loadTexture("two.jpg"),loadTexture("three.jpg"),loadTexture("four.jpg"),loadTexture("five.jpg"),loadTexture("six.jpg"),loadTexture("seven.jpg"),loadTexture("eight.jpg"),loadTexture("nine.jpg"),loadTexture("comma.jpg"),loadTexture("empty.jpg") };
+
 		//Load background
 		SDL_RenderCopy(gRenderer, background, NULL, NULL);
+		ClickToNumber(1, &comma, &AllNumber, numbers);
+		ClickToNumber(3, &comma, &AllNumber, numbers);
+		ClickToNumber(10, &comma, &AllNumber, numbers);
+		ClickToNumber(0, &comma, &AllNumber, numbers);
+		ClickToNumber(5, &comma, &AllNumber, numbers);
+
 		SDL_RenderPresent(gRenderer);
 		
 		//Quit SDL subsystems
 		freeTexture(background);
+		for (int i = 0; i < 12; i++)
+			freeTexture(numbers[i]);
 		//close();
 		_getch();
 	}
